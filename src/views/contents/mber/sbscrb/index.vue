85fec3fcee18478853b8e7f1ee2047d475611af9
zmklfg <jhpark@noncelab.com>
2020-11-04 16:13:30 +0900

    우편 수정중
    우편 수정중

--- src/views/contents/mber/sbscrb/index.vue
+++ src/views/contents/mber/sbscrb/index.vue
@@ -1,25 +1,32 @@
 <template>
   <chooseMberSbscrb v-if="step_1" @mberSe="chooseMberSbscrb"/>
-  <stplatAgre v-else-if="step_2" @certiNice="certiNiceSbscrb"/>
+  <stplatAgre v-else-if="step_2" @historyback="historyBackGo" @stplatAgre="stplatAgreSbscrb"/>
+  <sbscrbNice v-else-if="step_3" @nextStep="certiNiceSbscrb"/>
+  <indvSbscrb v-else-if="step_IndvSbscrb_4" @historyback="historyBackGo"/>
 </template>
 <script>
 import waves from '@/directive/waves'
 import chooseMberSbscrb from '@/views/contents/mber/sbscrb/sbscrb'
 import stplatAgre from '@/views/contents/mber/sbscrb/stplatAgre'
+import sbscrbNice from '@/views/contents/mber/sbscrb/sbscrbNice'
+import indvSbscrb from '@/views/contents/mber/sbscrb/indvSbscrb'
 // import chooseMberSbscrb from '@/views/contents/mber/sbscrb/sbscrb'
 export default {
   name: 'SbsCrbindex',
   directives: { waves },
   components: {
     chooseMberSbscrb,
-    stplatAgre
+    stplatAgre,
+    sbscrbNice,
+    indvSbscrb
   },
   data() {
     return {
       step_1: true,
       step_2: false,
       step_3: false,
-      step_4: false,
+      step_IndvSbscrb_4: false,
+      step_BizSbscrb_4: false,
       step_5: false,
       step_6: false,
       step_7: false,
@@ -33,15 +40,38 @@ export default {
 
   },
   methods: {
+    historyBackGo(num) {
+      switch (num) {
+        case 2:
+          this.step_1 = true
+          this.step_2 = false
+          break
+        case 4:
+          this.step_2 = true
+          this.step_IndvSbscrb_4 = false
+          this.step_BizSbscrb_4 = false
+          break
+      }
+    },
     chooseMberSbscrb(mberSeCd) {
       this.insertMberSbscrb.mberSeCd = mberSeCd
       this.step_1 = false
       this.step_2 = true
       console.log(this.insertMberSbscrb.mberSeCd)
     },
-    certiNiceSbscrb(subAgreCheakYn) {
+    stplatAgreSbscrb(subAgreCheakYn) {
       this.insertMberSbscrb.subAgreCheakYn = subAgreCheakYn
+      this.step_2 = false
+      this.step_3 = true
       console.log(this.insertMberSbscrb.subAgreCheakYn)
+    },
+    certiNiceSbscrb() {
+      if (this.insertMberSbscrb.mberSeCd === 'P') {
+        this.step_3 = false
+        this.step_IndvSbscrb_4 = true
+      } else {
+        console.log('화면이나 달아라 뻘짓하지말고')
+      }
     }
   }
 }
--- src/views/contents/mber/sbscrb/sbscrbNice.vue
+++ src/views/contents/mber/sbscrb/sbscrbNice.vue
@@ -5,7 +5,7 @@
       본인인증을 진행합니다.<br>
       본인인증 완료 후<br>
       회원가입을 진행 할 수 있습니다.
-      <a href="#" class="lpop_01_ok" @click="moveCreateIndvSbscrb()">확인</a>
+      <a href="#" class="lpop_01_ok" @click="moveNextSbscrb()">확인</a>
     </div>
   </div>
 </template>
@@ -13,28 +13,20 @@
 import waves from '@/directive/waves'
 
 export default {
-  name: 'IndvSbscrbNice',
+  name: 'SbscrbNice',
   directives: { waves },
-  props: {
-  // 유형(등록: add, 수정: edit)
-    subAgreCheakYn4: {
-      type: Boolean,
-      default: false
-    }
-  },
   data() {
     return {
-
     }
   },
   created() {
-    console.log(this.subAgreCheakYn_4)
   },
   methods: {
-    moveCreateIndvSbscrb() {
+    moveNextSbscrb() {
     // 원래는 나이스 인증 모듈 쪽으로 화면이 이동되거나 인증모듈을 실행시키는 함수임
     // 테스트 하기 위하여 지금은 일단 기본 회원 가입 쪽으로 넘기는 방향으로 하였음
-      this.$router.push({ name: 'createIndvSbscrb', params: { subAgreCheakYn_4: this.subAgreCheakYn_4 }})
+    // this.$router.push({ name: 'createIndvSbscrb', params: { subAgreCheakYn_4: this.subAgreCheakYn_4 }})
+      this.$emit('nextStep')
     }
   }
 }

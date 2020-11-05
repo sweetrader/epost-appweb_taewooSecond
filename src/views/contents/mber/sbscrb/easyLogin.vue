f04a4775b0c07c81eae5403bb9155b3f269c86c7
zmklfg <jhpark@noncelab.com>
2020-11-05 14:09:37 +0900

    회원가입 수정중 jhaprk
    회원가입 수정중 jhaprk

--- src/views/contents/mber/sbscrb/easyLogin.vue
+++ src/views/contents/mber/sbscrb/easyLogin.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="app-container">
     <header>
-      <a href="#" class="back"/><h1>회원가입</h1><a href="#" class="forward">건너뛰기</a>
+      <a href="#" class="back" @click="historyBack()"/><h1>회원가입</h1><a href="#" class="forward" @click="easyLoginPass()">건너뛰기</a>
     </header>
     <div class="easy_login_area">
       간편 로그인을 사용하시겠습니까?<br>간편 로그인에 사용할 방법을 선택해 주세요.<br><br>
       <a href="#" class="on onning"><div/><br>지문 등록</a>
       <a href="#" class="on"><div/><br>핀번호 등록</a>
-      <a href="#" class="next_btn2 itsok">확인</a>
+      <a href="#" class="next_btn2 itsok" @click="nextBtnActiveCheak()">확인</a>
     </div>
   </div>
 </template>
@@ -17,20 +17,32 @@ import waves from '@/directive/waves'
 export default {
   name: 'EasyLogin',
   directives: { waves },
+  props: {
+    index: {
+      type: Object,
+      default: () => ({})
+    }
+  },
   data() {
     return {
+      pinNoUseYn: this.index.pinNoUseYn,
+      bmtRcgnUseYn: this.index.bmtRcgnUseYn
     }
   },
   created() {
+    console.log(this.index)
   },
   methods: {
     historyBack() {
-      this.$emit('historyback', 2)
+      this.$emit('historyback', 5)
+    },
+    easyLoginPass() {
+      alert('그냥 건너뛰고 회원 가입 할거임?')
     },
     nextBtnActiveCheak() {
-
+      alert('트럼프 좃됨 ㅋㅋ')
+      this.$emit('easyLogin')
     }
-
   }
 }
 </script>
---
+++ src/views/contents/mber/sbscrb/sbscrbComplete.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app-container">
+    <header>
+      <h1>회원가입 완료</h1>
+    </header>
+    <div class="easy_login_area">
+      <div class="postman"/>
+      <br><br>
+      <span class="span_17"><b>{{ completeData.mberNm }}</b>님! 반갑습니다.</span><br>
+      <span class="span_14">우정물류공유플랫폼 <b>회원가입 완료</b>되었습니다.</span>
+      <a href="#" class="next_btn2 itsok" @click=" nextBtnActiveCheak()">로그인</a>
+    </div>
+  </div>
+</template>
+<script>
+import waves from '@/directive/waves'
+
+export default {
+  name: 'SbscrbComplete',
+  directives: { waves },
+  props: {
+    index: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      completeData: {
+        mberNm: this.index.mberNm
+      }
+    }
+  },
+  created() {
+
+  },
+  methods: {
+    nextBtnActiveCheak() {
+      this.$router.push({ name: 'login' })
+    }
+  }
+}
+</script>
+
